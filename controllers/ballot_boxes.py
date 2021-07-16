9eda9f75ed3ac5a6142b82adcf75090f517eab09
mvbraga@gmail.com <marvinbraga>
2021-07-16 20:13:02 -0300

    Controllers

---
+++ controllers/ballot_boxes.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+"""
+Ballot Boxes Controller Module
+Marcus Vinicius Braga, 2021.
+"""
+from utils.controllers.abstract import AbstractController
+
+
+class BallotBoxesController(AbstractController):
+    """ Controller """
+
+    def execute(self):
+        """ Executa o processo para urna eletrônica. """
+        return self
