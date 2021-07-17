11cef7056c98c69922218e7956e52ac99e60f86d
mvbraga@gmail.com <marvinbraga>
2021-07-17 01:33:26 -0300

    Ballot Boxes

--- controllers/ballot_boxes.py
+++ controllers/ballot_boxes.py
@@ -8,7 +8,25 @@ from utils.controllers.abstract import AbstractController
 
 class BallotBoxesController(AbstractController):
     """ Controller """
+    def __init__(self, model, *args, **kwargs):
+        self._kwargs = kwargs
+        self._args = args
+        self._blockchain = self._kwargs.get('blockchain')
+        self._model = model
+
+    @property
+    def model(self):
+        """ Return model """
+        return self._model
 
     def execute(self):
         """ Executa o processo para urna eletrônica. """
+        return self
+
+    def add_vote(self, vote):
+        """ Adiciona um novo voto. """
+        self._model.add_vote(vote)
+        if self._blockchain:
+            self._blockchain.add_transactions(vote.data)
+        vote.save()
         return self
