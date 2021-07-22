5a6f3405de5c5f24239d4906928af0504a401afd
mvbraga@gmail.com <marvinbraga>
2021-07-22 01:14:22 -0300

    Ballot Boxes
    Close #19

--- models/tests/test_models.py
+++ models/tests/test_models.py
@@ -42,7 +42,7 @@ def test_vote_instance():
     election = get_election(0)
     vote = Vote(1, 14, 1477, 'DF', election)
     vote_data = vote.data
-    test = ['1', '14', '1477', 'DF', '2021', 'PE', '1', '10', '116', '12000', '1']
+    test = ['1', '14', '1477', 'DF', 'None', '2021', 'PE', '1', '10', '116', '12000', '1']
     assert collections.Counter(vote_data) == collections.Counter(test)
 
 
